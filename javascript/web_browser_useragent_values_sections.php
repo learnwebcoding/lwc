df06b36e5854bc59e1813bbafb9d7c9b393c65c8
Steve Taylor <admin@learnwebcoding.com>
2020-11-18 00:27:26 -0500

    Update web_browser_useragent_values.php for release new version ED86, CH87, FF83, SM2.53.5

--- javascript/web_browser_useragent_values_sections.php
+++ javascript/web_browser_useragent_values_sections.php
@@ -4,7 +4,7 @@
 /* File: /web_server_root_directory/bom/web_browser_useragent_values_sections.php.
  * Purpose: Web page <section></section> sections.
  * Used in: /web_server_root_directory/bom/web_browser_useragent_values.php.
- * Last reviewed/updated: 12 Nov 2020.
+ * Last reviewed/updated: 17 Nov 2020.
  * Published: 21 Sep 2016. */
 return
    "<section>
@@ -1001,6 +1001,12 @@ return
 
      <p>Edge 86.0.622.68 64-bit<br />
      mozilla/5.0 (windows nt 10.0; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/86.0.4240.193 safari/537.36 edg/86.0.622.68</p>
+
+     <p>Edge 86.0.622.69 32-bit<br />
+     mozilla/5.0 (windows nt 10.0; wow64) applewebkit/537.36 (khtml, like gecko) chrome/86.0.4240.198 safari/537.36 edg/86.0.622.69</p>
+
+     <p>Edge 86.0.622.69 64-bit<br />
+     mozilla/5.0 (windows nt 10.0; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/86.0.4240.198 safari/537.36 edg/86.0.622.69</p>
 
     </section>
     <hr />
@@ -2480,6 +2486,12 @@ return
 
      <p>Firefox 82.0.3 64-bit<br />
      mozilla/5.0 (windows nt 10.0; win64; x64; rv:82.0) gecko/20100101 firefox/82.0</p>
+
+     <p>Firefox 83.0 32-bit<br />
+     mozilla/5.0 (windows nt 10.0; win64; x64; rv:83.0) gecko/20100101 firefox/83.0</p>
+
+     <p>Firefox 83.0 64-bit<br />
+     mozilla/5.0 (windows nt 10.0; win64; x64; rv:83.0) gecko/20100101 firefox/83.0</p>
 
     </section>
     <hr />
@@ -4068,6 +4080,12 @@ return
 
      <p>Chrome 86.0.4240.198 64-bit<br />
      mozilla/5.0 (windows nt 10.0; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/86.0.4240.198 safari/537.36</p>
+
+     <p>Chrome 87.0.4280.66 32-bit<br />
+     mozilla/5.0 (windows nt 10.0; wow64) applewebkit/537.36 (khtml, like gecko) chrome/87.0.4280.66 safari/537.36</p>
+
+     <p>Chrome 87.0.4280.66 64-bit<br />
+     mozilla/5.0 (windows nt 10.0; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/87.0.4280.66 safari/537.36</p>
 
     </section>
     <hr />
@@ -8328,6 +8346,26 @@ return
 
      <p>SeaMonkey 2.53.4 64-bit (Identify As SeaMonkey And Advertise Firefox Compatibility)<br />
      mozilla/5.0 (windows nt 10.0; win64; x64; rv:60.0) gecko/20100101 firefox/60.0 seamonkey/2.53.4</p>
+
+     <div class='note normal'>SeaMonkey 2.53.5 released when Firefox was at version 82.0.3.</div>
+
+     <p>SeaMonkey 2.53.5 32-bit (Identify As Firefox)<br />
+     mozilla/5.0 (windows nt 10.0; wow64; rv:60.0) gecko/20100101 firefox/60.0</p>
+
+     <p>SeaMonkey 2.53.5 32-bit (Identify As SeaMonkey)<br />
+     mozilla/5.0 (windows nt 10.0; wow64; rv:60.0) gecko/20100101 seamonkey/2.53.5</p>
+
+     <p>SeaMonkey 2.53.5 32-bit (Identify As SeaMonkey And Advertise Firefox Compatibility)<br />
+     mozilla/5.0 (windows nt 10.0; wow64; rv:60.0) gecko/20100101 firefox/60.0 seamonkey/2.53.5</p>
+
+     <p>SeaMonkey 2.53.5 64-bit (Identify As Firefox)<br />
+     mozilla/5.0 (windows nt 10.0; win64; x64; rv:60.0) gecko/20100101 firefox/60.0</p>
+
+     <p>SeaMonkey 2.53.5 64-bit (Identify As SeaMonkey)<br />
+     mozilla/5.0 (windows nt 10.0; win64; x64; rv:60.0) gecko/20100101 seamonkey/2.53.5</p>
+
+     <p>SeaMonkey 2.53.5 64-bit (Identify As SeaMonkey And Advertise Firefox Compatibility)<br />
+     mozilla/5.0 (windows nt 10.0; win64; x64; rv:60.0) gecko/20100101 firefox/60.0 seamonkey/2.53.5</p>
 
     </section>
     <hr />
--- misc/whats_new_sections.php
+++ misc/whats_new_sections.php
@@ -4,7 +4,7 @@
 /* File: /web_server_root_directory/misc/whats_new_sections.php.
  * Purpose: Web page <section></section> sections.
  * Used in: /web_server_root_directory/misc/whats_new.php.
- * Last reviewed/updated: 12 Nov 2020.
+ * Last reviewed/updated: 17 Nov 2020.
  * Published: 21 Sep 2016. */
 return
    "<!-- This web page does not have an h2-h6 element in the section element. To avoid W3C Markup Validation Service (http://validator.w3.org/) warning, not error (Warning: Section lacks heading. Consider using h2-h6 elements to add identifying headings to all sections.), comment out the section element. Same for index.php, sitemap.php, glossary.php, and whats_new.php.
@@ -12,7 +12,7 @@ return
 -->
 <!-- Resources are \"Added\". Instructional pages are \"Published\". Always keep when a page is \"Added/Published\". When a page is \"Major update\" remove prior \"Updated/Major update\". When a page is \"Updated\" remove prior \"Updated\". When a page is \"Updated\" keep prior \"Major update\". When a page is \"Updated/Major update\" remove prior \"Renamed\". When a page is \"Renamed\" without being \"Updated\" keep prior \"Updated/Major update\". When a page is \"Renamed\" and \"Updated\" remove prior \"Updated\" and keep prior \"Major update\". When a page is \"Renamed\" rename all refs to the page (including Updated/Major update/Added/Published) to the new name. When a page is \"Deleted\" remove prior \"Published\", remove prior \"Updated\, and mention page \"Deleted.\". When a page is \"Moved to discontinued\" keep prior \"Published\", remove prior \"Updated\", and do not mention page \"Moved to discontinued\". -->
      <ul class='link-list'>
-      <li><a href='/javascript/web_browser_useragent_values.php'>Web Browser User-Agent String Values (learnwebcoding.com)</a>: Updated: 12 Nov 2020.</li>
+      <li><a href='/javascript/web_browser_useragent_values.php'>Web Browser User-Agent String Values (learnwebcoding.com)</a>: Updated: 17 Nov 2020.</li>
       <li><a href='/css/css_testing_ground/visualize_user_agent_or_bootstrap_styles.html'>Visualize User Agent Or Bootstrap Styles (learnwebcoding.com)</a>: Updated: 19 Sep 2020.</li>
       <li><a href='https://jquery.com/'>jQuery Core (jquery.com)</a>: Updated from 3.4.1 to 3.5.1: 22 Aug 2020.</li>
       <li><a href='/index.php'>Learn Web Coding Home Page (learnwebcoding.com)</a>: Updated: 22 Aug 2020.</li>
