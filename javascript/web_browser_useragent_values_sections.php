f3889929c7fb5b6c44734602926ee7b8aa5f4085
Steve Taylor <admin@learnwebcoding.com>
2021-05-25 22:33:28 -0400

    Update web_browser_useragent_values.php for release new version CH91

--- javascript/web_browser_useragent_values_sections.php
+++ javascript/web_browser_useragent_values_sections.php
@@ -4,7 +4,7 @@
 /* File: /web_server_root_directory/bom/web_browser_useragent_values_sections.php.
  * Purpose: Web page <section></section> sections.
  * Used in: /web_server_root_directory/bom/web_browser_useragent_values.php.
- * Last reviewed/updated: 24 May 2021.
+ * Last reviewed/updated: 25 May 2021.
  * Published: 21 Sep 2016. */
 return
    "<section>
@@ -4525,6 +4525,12 @@ return
 
      <p>Chrome 90.0.4430.212 64-bit<br />
      mozilla/5.0 (windows nt 10.0; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/90.0.4430.212 safari/537.36</p>
+
+     <p>Chrome 91.0.4472.77 32-bit<br />
+     mozilla/5.0 (windows nt 10.0; wow64) applewebkit/537.36 (khtml, like gecko) chrome/91.0.4472.77 safari/537.36</p>
+
+     <p>Chrome 91.0.4472.77 64-bit<br />
+     mozilla/5.0 (windows nt 10.0; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/91.0.4472.77 safari/537.36</p>
 
     </section>
     <hr />
