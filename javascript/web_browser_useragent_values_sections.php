635c9119c38c98c0edf3f25ead5b3a7f93c88ed3
Steve Taylor <admin@learnwebcoding.com>
2022-12-09 16:10:50 -0500

    Update web_browser_useragent_values.php for release new version ED108 and VV5.6

--- javascript/web_browser_useragent_values_sections.php
+++ javascript/web_browser_useragent_values_sections.php
@@ -4,7 +4,7 @@
 /* File: /web_server_root_directory/bom/web_browser_useragent_values_sections.php.
  * Purpose: Web page <section></section> sections.
  * Used in: /web_server_root_directory/bom/web_browser_useragent_values.php.
- * Last reviewed/updated: 07 Dec 2022.
+ * Last reviewed/updated: 09 Dec 2022.
  * Published: 21 Sep 2016. */
 return
    "<section>
@@ -1744,6 +1744,9 @@ return
 
      <p>Edge 108.0.1462.42 64-bit<br />
      mozilla/5.0 (windows nt 10.0; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/108.0.0.0 safari/537.36 edg/108.0.1462.42</p>
+
+     <p>Edge 108.0.1462.46 64-bit<br />
+     mozilla/5.0 (windows nt 10.0; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/108.0.0.0 safari/537.36 edg/108.0.1462.46</p>
 
     </section>
     <hr />
@@ -9786,6 +9789,9 @@ return
 
      <p>Vivaldi 5.5.2805.50 64-bit<br />
      mozilla/5.0 (windows nt 10.0; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/106.0.0.0 safari/537.36</p>
+
+     <p>Vivaldi 5.6.2867.40 64-bit<br />
+     mozilla/5.0 (windows nt 10.0; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/108.0.0.0 safari/537.36</p>
 
     </section>
     <hr />
