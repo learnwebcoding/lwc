baf726b5ad96ae29b2ff2e7221a06e8cd9b0b8b6
Steve Taylor <admin@learnwebcoding.com>
2023-02-14 17:21:17 -0500

    Update web_browser_useragent_values.php for release new version FF110 and CH110

--- javascript/web_browser_useragent_values_sections.php
+++ javascript/web_browser_useragent_values_sections.php
@@ -4,7 +4,7 @@
 /* File: /web_server_root_directory/bom/web_browser_useragent_values_sections.php.
  * Purpose: Web page <section></section> sections.
  * Used in: /web_server_root_directory/bom/web_browser_useragent_values.php.
- * Last reviewed/updated: 13 Feb 2023.
+ * Last reviewed/updated: 14 Feb 2023.
  * Published: 21 Sep 2016. */
 return
    "<section>
@@ -3493,6 +3493,9 @@ return
      <p>Firefox 109.0.1 64-bit<br />
      mozilla/5.0 (windows nt 10.0; win64; x64; rv:109.0) gecko/20100101 firefox/109.0</p>
 
+     <p>Firefox 110.0 64-bit<br />
+     mozilla/5.0 (windows nt 10.0; win64; x64; rv:109.0) gecko/20100101 firefox/110.0</p>
+
     </section>
     <hr />
     <section>
@@ -5448,6 +5451,9 @@ return
      mozilla/5.0 (windows nt 10.0; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/110.0.0.0 safari/537.36</p>
 
      <p>Chrome 110.0.5481.97 64-bit<br />
+     mozilla/5.0 (windows nt 10.0; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/110.0.0.0 safari/537.36</p>
+
+     <p>Chrome 110.0.5481.100 64-bit<br />
      mozilla/5.0 (windows nt 10.0; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/110.0.0.0 safari/537.36</p>
 
     </section>
