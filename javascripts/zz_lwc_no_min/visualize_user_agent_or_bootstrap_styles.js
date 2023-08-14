f52b6c6247b12cdde4620e9745312c482f08301e
Steve Taylor <admin@learnwebcoding.com>
2023-08-14 00:34:53 -0400

    For visualize_user_agent_or_bootstrap_styles.html, copy two files accidently not copied when they were lasted updated.

--- javascripts/zz_lwc_no_min/visualize_user_agent_or_bootstrap_styles.js
+++ javascripts/zz_lwc_no_min/visualize_user_agent_or_bootstrap_styles.js
@@ -3,7 +3,7 @@
 /* File: /web_server_root_directory/javascripts/lwc/visualize_user_agent_or_bootstrap_styles.js
  * Purpose: Change external style sheet when click form selectbox (dropdown) option.
  * Used in: visualize_user_agent_or_bootstrap_styles.html.
- * Last reviewed/updated: 04 Mar 2019.
+ * Last reviewed/updated: 19 Sep 2020.
  * Published: 27 Nov 2018.
  * Unobtrusive: 1.) decouple HTML/JavaScript: a.) no register JavaScript event handlers in HTML via HTML attributes (eg, onload and onclick), and b.) no embed JavaScripts in HTML via HTML script element; 2.) as reasonable, decouple CSS/JavaScript. Eg, as reasonable, use JavaScript to change HTML class attribute value assignments (loose coupling), not use JavaScript to change JavaScript style object CSS property value assignments (tight coupling); and 3.) no define JavaScript variables and functions on JavaScript global scope.
  * Web browser support: IE11+, ED12+, FF19+, CH38+, OP25+, which corresponds to Map object support.
@@ -26,12 +26,14 @@ var ChangeExtSSUtil = {
   mapOptionValueToSsUri.set("bs413", "https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"); // Per https://getbootstrap.com/docs/4.1/getting-started/introduction/.
   mapOptionValueToSsUri.set("bs421", "https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"); // Per https://getbootstrap.com/docs/4.2/getting-started/introduction/.
   mapOptionValueToSsUri.set("bs431", "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"); // Per https://getbootstrap.com/docs/4.3/getting-started/introduction/.
+  mapOptionValueToSsUri.set("bs441", "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"); // Per https://getbootstrap.com/docs/4.4/getting-started/introduction/.
+  mapOptionValueToSsUri.set("bs452", "https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"); // Per https://getbootstrap.com/docs/4.5/getting-started/introduction/.
   // Get reference to link element.
   var linkElement = document.getElementById("linkElementId");
   // For when a selectbox option is selected, get reference to selectbox.
   var eventTarget = event.target; // <select>...</select>.
   // For the selected selectbox option, get value attribute value.
-  var selectedOption = eventTarget.value; // "userAgent", "bs337", "bs340", "bs413", or "bs421".
+  var selectedOption = eventTarget.value; // "userAgent", "bs337", "bs341", "bs400", "bs413", "bs421", "bs431", "bs441", or "bs452".
   // Using selected option as key, get mapOptionValueToSsUri map value and set on link element href property. This changes the external style sheet, which changes the web page styles.
   linkElement.href = mapOptionValueToSsUri.get(selectedOption);
  },
