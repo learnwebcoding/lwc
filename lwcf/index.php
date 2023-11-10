077ff7b23df1d768df421d8e5d297576d1da0892
Steve Taylor <admin@learnwebcoding.com>
2023-11-10 01:10:14 -0500

    On 01Nov23 uninstalled LWC Forums. A couple years ago uninstalled LWC Blog. See Description for additional info...
    Here now starting to delete unneeded directories related to LWC Blog (wp) and LWC Forums (phpbb) as they are no longer needed. However, to serve as placeholders, am also creating new LWC Blog and new LWC Forums in new directories (lwcb and lwcf, respectively) from Learn Web Coding Home Template Download.
    
    Changes in this commit/push are related to creating new LWC Blog (lwcb) and new LWC Forums (lwcf) from Learn Web Coding Home Template Download.

---
+++ lwcf/index.php
@@ -0,0 +1,19 @@
+<?php
+/* -------------------- INTRODUCTION -------------------- */
+
+/* File: /web_server_root_directory/index.php.
+ * Purpose: Web page file. LWC Forums home page. A variation of /web_server_root_directory/templates/web_page.php.
+ * Used in: No other file.
+ * Last reviewed/updated: 09 Nov 2023.
+ * Published: 21 Sep 2016. */
+error_reporting(E_ALL);
+ini_set('display_errors', '0'); // To display errors, change '0' to '1'.
+include_once "Web_Page.class.php";
+$webPage = new Web_Page();
+$webPage->head_title = "Learn Web Coding Forums"; // No markup in title tags.
+$webPage->head_meta_keywords = "Learn Web Coding Forums, home page"; // Cap words as if used in sentence. No markup in Keywords.
+$webPage->head_meta_description = "The Learn Web Coding Forums home page."; // Sentence and/or structured data less than 160 char per Google. No markup in Description.
+$webPage->body_main_sections = include_once "index_sections.php";
+$webPage->body_script_javascripts = include_once "javascripts_home_page.php";
+$html = include_once "template_home_page.php";
+echo $html;
---
+++ lwcf/index_sections.php
@@ -0,0 +1,36 @@
+<?php
+/* -------------------- INTRODUCTION -------------------- */
+
+/* File: /web_server_root_directory/index_sections.php.
+ * Purpose: Web page <section></section> sections.
+ * Used in: /web_server_root_directory/index.php.
+ * Last reviewed/updated: 09 Nov 2023.
+ * Published: 21 Sep 2016. */
+return
+  "<!-- This web page does not have an h2-h6 element in the section element. To avoid W3C Markup Validation Service (http://validator.w3.org/) warning, not error (Warning: Section lacks heading. Consider using h2-h6 elements to add identifying headings to all sections.), comment out the section element. Same for index.php, sitemap.php, glossary.php, and whats_new.php.
+   <section>
+-->
+    <div class='container-fluid'><!-- container-fluid from Bootstrap. -->
+     <div class='home-page-main'>
+      <div class='home-page-main-subtitle-row'>
+       <div class='home-page-main-subtitle'>Learn Web Coding Forums</div>
+      </div>
+      <div class='home-page-main-fa-row'>
+       <div class='row'>
+        <div class='col-sm-4 home-page-main-fa-panel'>
+         <div></div>
+        </div>
+        <div class='col-sm-4 home-page-main-fa-panel'>
+         <div class='home-page-main-fa-title'>Deleted. Now serves as placeholder.</div>
+         <div></div>
+        </div>
+        <div class='col-sm-4 home-page-main-fa-panel'>
+         <div></div>
+        </div>
+       </div>
+      </div>
+     </div>
+    </div><!-- Close main section div class='container-fluid'. -->
+<!--
+   </section>
+-->";
