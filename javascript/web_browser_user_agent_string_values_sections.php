61ccff16b952c323cf849a758185d75cefa9e7ec
Steve Taylor <admin@learnwebcoding.com>
2024-01-23 16:18:49 -0500

    Update web_browser_user_agent_string_values.php for release new version ED120, FF122, CH121, OP106, VV6.5

--- javascript/web_browser_user_agent_string_values_sections.php
+++ javascript/web_browser_user_agent_string_values_sections.php
@@ -4,7 +4,7 @@
 /* File: /web_server_root_directory/bom/web_browser_user_agent_string_values_sections.php.
  * Purpose: Web page <section></section> sections.
  * Used in: /web_server_root_directory/bom/web_browser_user_agent_string_values.php.
- * Last reviewed/updated: 16 Jan 2024.
+ * Last reviewed/updated: 23 Jan 2024.
  * Published: 21 Sep 2016. */
 return
    "<section>
@@ -2040,6 +2040,9 @@ return
      <p>Edge 120.0.2210.133 64-bit<br />
      mozilla/5.0 (windows nt 10.0; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/120.0.0.0 safari/537.36 edg/120.0.0.0</p>
 
+     <p>Edge 120.0.2210.144 64-bit<br />
+     mozilla/5.0 (windows nt 10.0; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/120.0.0.0 safari/537.36 edg/120.0.0.0</p>
+
     </section>
     <hr />
     <section>
@@ -3807,6 +3810,9 @@ return
 
      <p>Firefox 121.0.1 64-bit<br />
      mozilla/5.0 (windows nt 10.0; win64; x64; rv:121.0) gecko/20100101 firefox/121.0</p>
+
+     <p>Firefox 122.0 64-bit<br />
+     mozilla/5.0 (windows nt 10.0; win64; x64; rv:122.0) gecko/20100101 firefox/122.0</p>
 
     </section>
     <hr />
@@ -5894,6 +5900,9 @@ return
 
      <p>Chrome 120.0.6099.225 64-bit<br />
      mozilla/5.0 (windows nt 10.0; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/120.0.0.0 safari/537.36</p>
+
+     <p>Chrome 121.0.6167.86 64-bit<br />
+     mozilla/5.0 (windows nt 10.0; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/121.0.0.0 safari/537.36</p>
 
     </section>
     <hr />
@@ -9446,6 +9455,9 @@ return
      <p>Opera 106.0.4998.41 64-bit<br />
      mozilla/5.0 (windows nt 10.0; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/120.0.0.0 safari/537.36 opr/106.0.0.0</p>
 
+     <p>Opera 106.0.4998.52 64-bit<br />
+     mozilla/5.0 (windows nt 10.0; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/120.0.0.0 safari/537.36 opr/106.0.0.0</p>
+
     </section>
     <hr />
     <section>
@@ -10699,6 +10711,9 @@ return
      mozilla/5.0 (windows nt 10.0; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/120.0.0.0 safari/537.36</p>
 
      <p>Vivaldi 6.5.3206.53 64-bit<br />
+     mozilla/5.0 (windows nt 10.0; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/120.0.0.0 safari/537.36</p>
+
+     <p>Vivaldi 6.5.3206.55 64-bit<br />
      mozilla/5.0 (windows nt 10.0; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/120.0.0.0 safari/537.36</p>
 
     </section>
--- misc/whats_new_sections.php
+++ misc/whats_new_sections.php
@@ -4,7 +4,7 @@
 /* File: /web_server_root_directory/misc/whats_new_sections.php.
  * Purpose: Web page <section></section> sections.
  * Used in: /web_server_root_directory/misc/whats_new.php.
- * Last reviewed/updated: 16 Jan 2024.
+ * Last reviewed/updated: 23 Jan 2024.
  * Published: 21 Sep 2016. */
 return
    "<!-- This web page does not have an h2-h6 element in the section element. To avoid W3C Markup Validation Service (http://validator.w3.org/) warning, not error (Warning: Section lacks heading. Consider using h2-h6 elements to add identifying headings to all sections.), comment out the section element. Same for index.php, sitemap.php, glossary.php, and whats_new.php.
@@ -12,7 +12,7 @@ return
 -->
 <!-- Resources are \"Added\". Instructional pages are \"Published\". Always keep when a page is \"Added/Published\". When a page is \"Major update\" remove prior \"Updated/Major update\". When a page is \"Updated\" remove prior \"Updated\". When a page is \"Updated\" keep prior \"Major update\". When a page is \"Updated/Major update\" remove prior \"Renamed\". When a page is \"Renamed\" without being \"Updated\" keep prior \"Updated/Major update\". When a page is \"Renamed\" and \"Updated\" remove prior \"Updated\" and keep prior \"Major update\". When a page is \"Renamed\" rename all refs to the page (including Updated/Major update/Added/Published) to the new name. When a page is \"Deleted\" remove prior \"Published\", remove prior \"Updated\, and mention page \"Deleted.\". When a page is \"Moved to discontinued\" keep prior \"Published\", remove prior \"Updated\", and do not mention page \"Moved to discontinued\". -->
      <ul class='link-list'>
-      <li><a href='/javascript/web_browser_user_agent_string_values.php'>Web Browser User Agent String Values (learnwebcoding.com)</a>: Updated: 16 Jan 2024.</li>
+      <li><a href='/javascript/web_browser_user_agent_string_values.php'>Web Browser User Agent String Values (learnwebcoding.com)</a>: Updated: 23 Jan 2024.</li>
       <li><a href='/misc/versions_ie_ed_windows.php'>Versions Of Internet Explorer And Edge Included With And Supported By Windows 98/ME/2000/XP/Vista/7/8/8.1/10/11 (learnwebcoding.com)</a>: Updated: 06 Nov 2023.</li>
       <li><a href='/css/css_font_compare.php'>Select And Compare CSS <code>font</code> Property Values Side By Side Online (learnwebcoding.com)</a>: Updated: 22 Aug 2023.</li>
       <li><a href='/css/css_font_compare.php'>Select And Compare CSS <code>font</code> Property Values Side By Side Online (learnwebcoding.com)</a>: Major update: 17 Aug 2023.</li>
