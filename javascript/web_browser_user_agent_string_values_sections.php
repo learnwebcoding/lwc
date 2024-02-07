744d208837b16971196c02717f4d5f5faae120ba
Steve Taylor <admin@learnwebcoding.com>
2024-02-07 02:20:15 -0500

    Update web_browser_user_agent_string_values.php for release new version ED121, FF122, CH121

--- javascript/web_browser_user_agent_string_values_sections.php
+++ javascript/web_browser_user_agent_string_values_sections.php
@@ -4,7 +4,7 @@
 /* File: /web_server_root_directory/bom/web_browser_user_agent_string_values_sections.php.
  * Purpose: Web page <section></section> sections.
  * Used in: /web_server_root_directory/bom/web_browser_user_agent_string_values.php.
- * Last reviewed/updated: 31 Jan 2024.
+ * Last reviewed/updated: 06 Feb 2024.
  * Published: 21 Sep 2016. */
 return
    "<section>
@@ -2052,6 +2052,12 @@ return
      <p>Edge 121.0.2277.83 64-bit<br />
      mozilla/5.0 (windows nt 10.0; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/121.0.0.0 safari/537.36 edg/121.0.0.0</p>
 
+     <p>Edge 121.0.2277.98 64-bit<br />
+     mozilla/5.0 (windows nt 10.0; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/121.0.0.0 safari/537.36 edg/121.0.0.0</p>
+
+     <p>Edge 121.0.2277.106 64-bit<br />
+     mozilla/5.0 (windows nt 10.0; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/121.0.0.0 safari/537.36 edg/121.0.0.0</p>
+
     </section>
     <hr />
     <section>
@@ -3821,6 +3827,9 @@ return
      mozilla/5.0 (windows nt 10.0; win64; x64; rv:121.0) gecko/20100101 firefox/121.0</p>
 
      <p>Firefox 122.0 64-bit<br />
+     mozilla/5.0 (windows nt 10.0; win64; x64; rv:122.0) gecko/20100101 firefox/122.0</p>
+
+     <p>Firefox 122.0.1 64-bit<br />
      mozilla/5.0 (windows nt 10.0; win64; x64; rv:122.0) gecko/20100101 firefox/122.0</p>
 
     </section>
@@ -5914,6 +5923,9 @@ return
      mozilla/5.0 (windows nt 10.0; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/121.0.0.0 safari/537.36</p>
 
      <p>Chrome 121.0.6167.140 64-bit<br />
+     mozilla/5.0 (windows nt 10.0; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/121.0.0.0 safari/537.36</p>
+
+     <p>Chrome 121.0.6167.161 64-bit<br />
      mozilla/5.0 (windows nt 10.0; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/121.0.0.0 safari/537.36</p>
 
     </section>
