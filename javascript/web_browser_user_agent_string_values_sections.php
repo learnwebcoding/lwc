3aafb88edb19c697693a45a4fee62ad1a1797496
Steve Taylor <admin@learnwebcoding.com>
2024-02-22 18:19:26 -0500

    Update web_browser_user_agent_string_values.php for release new version CH122 and OP107

--- javascript/web_browser_user_agent_string_values_sections.php
+++ javascript/web_browser_user_agent_string_values_sections.php
@@ -4,7 +4,7 @@
 /* File: /web_server_root_directory/bom/web_browser_user_agent_string_values_sections.php.
  * Purpose: Web page <section></section> sections.
  * Used in: /web_server_root_directory/bom/web_browser_user_agent_string_values.php.
- * Last reviewed/updated: 21 Feb 2024.
+ * Last reviewed/updated: 22 Feb 2024.
  * Published: 21 Sep 2016. */
 return
    "<section>
@@ -5946,6 +5946,9 @@ return
      <p>Chrome 122.0.6261.58 64-bit<br />
      mozilla/5.0 (windows nt 10.0; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/122.0.0.0 safari/537.36</p>
 
+     <p>Chrome 122.0.6261.70 64-bit<br />
+     mozilla/5.0 (windows nt 10.0; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/122.0.0.0 safari/537.36</p>
+
     </section>
     <hr />
     <section>
@@ -9510,6 +9513,9 @@ return
      mozilla/5.0 (windows nt 10.0; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/121.0.0.0 safari/537.36 opr/107.0.0.0</p>
 
      <p>Opera 107.0.5045.21 64-bit<br />
+     mozilla/5.0 (windows nt 10.0; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/121.0.0.0 safari/537.36 opr/107.0.0.0</p>
+
+     <p>Opera 107.0.5045.36 64-bit<br />
      mozilla/5.0 (windows nt 10.0; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/121.0.0.0 safari/537.36 opr/107.0.0.0</p>
 
     </section>
