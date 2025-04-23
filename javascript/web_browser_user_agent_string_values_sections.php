3ba8128392b9d9e627b2aa73a8de2f3131b61772
Steve Taylor <admin@learnwebcoding.com>
2025-04-23 01:55:33 -0400

    Update web_browser_user_agent_string_values.php for release new version CH135

--- javascript/web_browser_user_agent_string_values_sections.php
+++ javascript/web_browser_user_agent_string_values_sections.php
@@ -4,7 +4,7 @@
 /* File: /web_server_root_directory/bom/web_browser_user_agent_string_values_sections.php.
  * Purpose: Web page <section></section> sections.
  * Used in: /web_server_root_directory/bom/web_browser_user_agent_string_values.php.
- * Last reviewed/updated: 18 Apr 2025.
+ * Last reviewed/updated: 22 Apr 2025.
  * Published: 21 Sep 2016. */
 return
    "<section>
@@ -6526,6 +6526,9 @@ return
      mozilla/5.0 (windows nt 10.0; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/135.0.0.0 safari/537.36</p>
 
      <p>Chrome 135.0.7049.96 64-bit<br />
+     mozilla/5.0 (windows nt 10.0; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/135.0.0.0 safari/537.36</p>
+
+     <p>Chrome 135.0.7049.115 64-bit<br />
      mozilla/5.0 (windows nt 10.0; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/135.0.0.0 safari/537.36</p>
 
     </section>
