8f5e5d6dc469da257ef4e8963426c6bfa4e4b08f
Steve Taylor <admin@learnwebcoding.com>
2025-06-11 03:15:46 -0400

    Update web_browser_user_agent_string_values.php for release new version ED137, FF139, CH137, OP119, VV7.4 and SM2.53

--- javascript/web_browser_user_agent_string_values_sections.php
+++ javascript/web_browser_user_agent_string_values_sections.php
@@ -4,7 +4,7 @@
 /* File: /web_server_root_directory/bom/web_browser_user_agent_string_values_sections.php.
  * Purpose: Web page <section></section> sections.
  * Used in: /web_server_root_directory/bom/web_browser_user_agent_string_values.php.
- * Last reviewed/updated: 02 Jun 2025.
+ * Last reviewed/updated: 10 Jun 2025.
  * Published: 21 Sep 2016. */
 return
    "<section>
@@ -2365,6 +2365,12 @@ return
      <p>Edge 137.0.3296.58 64-bit<br />
      mozilla/5.0 (windows nt 10.0; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/137.0.0.0 safari/537.36 edg/137.0.0.0</p>
 
+     <p>Edge 137.0.3296.62 64-bit<br />
+     mozilla/5.0 (windows nt 10.0; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/137.0.0.0 safari/537.36 edg/137.0.0.0</p>
+
+     <p>Edge 137.0.3296.68 64-bit<br />
+     mozilla/5.0 (windows nt 10.0; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/137.0.0.0 safari/537.36 edg/137.0.0.0</p>
+
     </section>
     <hr />
     <section>
@@ -4291,6 +4297,11 @@ return
      mozilla/5.0 (windows nt 10.0; win64; x64; rv:139.0) gecko/20100101 firefox/139.0</p>
 
      <p>Firefox 139.0.1 64-bit<br />
+     mozilla/5.0 (windows nt 10.0; win64; x64; rv:139.0) gecko/20100101 firefox/139.0</p>
+
+     <div class='note normal'>The version of Firefox skipped from 139.0.1 to 139.0.4.</div>
+
+     <p>Firefox 139.0.4 64-bit<br />
      mozilla/5.0 (windows nt 10.0; win64; x64; rv:139.0) gecko/20100101 firefox/139.0</p>
 
     </section>
@@ -6588,6 +6599,9 @@ return
      mozilla/5.0 (windows nt 10.0; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/137.0.0.0 safari/537.36</p>
 
      <p>Chrome 137.0.7151.69 64-bit<br />
+     mozilla/5.0 (windows nt 10.0; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/137.0.0.0 safari/537.36</p>
+
+     <p>Chrome 137.0.7151.104 64-bit<br />
      mozilla/5.0 (windows nt 10.0; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/137.0.0.0 safari/537.36</p>
 
     </section>
@@ -10375,6 +10389,9 @@ return
      <p>Opera 119.0.5497.56 64-bit<br />
      mozilla/5.0 (windows nt 10.0; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/134.0.0.0 safari/537.36 opr/119.0.0.0</p>
 
+     <p>Opera 119.0.5497.70 64-bit<br />
+     mozilla/5.0 (windows nt 10.0; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/134.0.0.0 safari/537.36 opr/119.0.0.0</p>
+
     </section>
     <hr />
     <section>
@@ -11864,6 +11881,9 @@ return
      <p>Vivaldi 7.4.3684.43 64-bit<br />
      mozilla/5.0 (windows nt 10.0; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/136.0.0.0 safari/537.36</p>
 
+     <p>Vivaldi 7.4.3684.46 64-bit<br />
+     mozilla/5.0 (windows nt 10.0; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/136.0.0.0 safari/537.36</p>
+
     </section>
     <hr />
     <section>
@@ -12927,6 +12947,17 @@ return
 
      <p>SeaMonkey 2.53.20 64-bit (Identify As SeaMonkey And Advertise Firefox Compatibility)<br />
      mozilla/5.0 (windows nt 10.0; win64; x64; rv:128.0) gecko/20100101 firefox/128.0 seamonkey/2.53.20</p>
+
+     <div class='note normal'>SeaMonkey 2.53.21 released when Firefox was at version 139.0.1.</div>
+
+     <p>SeaMonkey 2.53.21 64-bit (Identify As Firefox)<br />
+     mozilla/5.0 (windows nt 10.0; win64; x64; rv:128.0) gecko/20100101 firefox/128.0</p>
+
+     <p>SeaMonkey 2.53.21 64-bit (Identify As SeaMonkey)<br />
+     mozilla/5.0 (windows nt 10.0; win64; x64; rv:128.0) gecko/20100101 seamonkey/2.53.21</p>
+
+     <p>SeaMonkey 2.53.21 64-bit (Identify As SeaMonkey And Advertise Firefox Compatibility)<br />
+     mozilla/5.0 (windows nt 10.0; win64; x64; rv:128.0) gecko/20100101 firefox/128.0 seamonkey/2.53.21</p>
 
     </section>
     <hr />
