5f3ed60269c980b6a5b4471bde76964915a58e03
Steve Taylor <admin@learnwebcoding.com>
2025-10-17 01:57:31 -0400

    Update web_browser_user_agent_string_values.php for release new version ED140, FF144 and OP122

--- javascript/web_browser_user_agent_string_values_sections.php
+++ javascript/web_browser_user_agent_string_values_sections.php
@@ -4,7 +4,7 @@
 /* File: /web_server_root_directory/bom/web_browser_user_agent_string_values_sections.php.
  * Purpose: Web page <section></section> sections.
  * Used in: /web_server_root_directory/bom/web_browser_user_agent_string_values.php.
- * Last reviewed/updated: 14 Oct 2025.
+ * Last reviewed/updated: 16 Oct 2025.
  * Published: 21 Sep 2016. */
 return
    "<section>
@@ -2462,6 +2462,9 @@ return
      <p>Edge 140.0.3485.110 64-bit<br />
      mozilla/5.0 (windows nt 10.0; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/140.0.0.0 safari/537.36 edg/140.0.0.0</p>
 
+     <p>Edge 140.0.3485.119 64-bit<br />
+     mozilla/5.0 (windows nt 10.0; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/140.0.0.0 safari/537.36 edg/140.0.0.0</p>
+
      <p>Edge 141.0.3537.57 64-bit<br />
      mozilla/5.0 (windows nt 10.0; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/141.0.0.0 safari/537.36 edg/141.0.0.0</p>
 
@@ -4442,6 +4445,9 @@ return
 
      <p>Firefox 143.0.4 64-bit<br />
      mozilla/5.0 (windows nt 10.0; win64; x64; rv:143.0) gecko/20100101 firefox/143.0</p>
+
+     <p>Firefox 144.0 64-bit<br />
+     mozilla/5.0 (windows nt 10.0; win64; x64; rv:144.0) gecko/20100101 firefox/144.0</p>
 
     </section>
     <hr />
@@ -10637,6 +10643,9 @@ return
      mozilla/5.0 (windows nt 10.0; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/138.0.0.0 safari/537.36 opr/122.0.0.0</p>
 
      <p>Opera 122.0.5643.92 64-bit<br />
+     mozilla/5.0 (windows nt 10.0; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/138.0.0.0 safari/537.36 opr/122.0.0.0</p>
+
+     <p>Opera 122.0.5643.142 64-bit<br />
      mozilla/5.0 (windows nt 10.0; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/138.0.0.0 safari/537.36 opr/122.0.0.0</p>
 
     </section>
